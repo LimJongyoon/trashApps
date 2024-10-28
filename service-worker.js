f8364caee0ffbc59ebcea8acb9e04ed2c5c4de20
Limjongyoon <limjy.kor@gmail.com>
2024-10-28 19:33:22 +0900

    Serviceworker

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'trash-apps-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/favicons/android-chrome-192x192.png',
+    '/favicons/android-chrome-512x512.png'
+  ];  
+
+// 설치 이벤트 - 초기 캐싱
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// 요청 인터셉트하여 캐시 사용
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
+
+// 활성화 이벤트 - 이전 캐시 삭제
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
