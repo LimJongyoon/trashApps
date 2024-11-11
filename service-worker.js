f5004e6dcad2495b840c7c2070875c19f3246fb7
Limjongyoon <limjy.kor@gmail.com>
2024-11-11 20:30:52 +0900

    0019.5

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'TrashApp 0.19.2'; // 버전 업데이트
+const CACHE_NAME = 'TrashApp 0.19.4';
 const urlsToCache = [
     '/trashApps/',
     '/trashApps/index.html',
@@ -6,16 +6,14 @@ const urlsToCache = [
     '/trashApps/favicons/android-chrome-512x512.png'
 ];
 
-// 설치 이벤트 - 초기 캐싱 및 즉시 활성화
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => cache.addAll(urlsToCache))
   );
-  self.skipWaiting(); // 설치 후 즉시 활성화
+  self.skipWaiting(); 
 });
 
-// 요청 인터셉트하여 캐시 사용
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
@@ -23,7 +21,6 @@ self.addEventListener('fetch', (event) => {
   );
 });
 
-// 활성화 이벤트 - 이전 캐시 삭제
 self.addEventListener('activate', (event) => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
@@ -37,5 +34,5 @@ self.addEventListener('activate', (event) => {
       );
     })
   );
-  self.clients.claim(); // 활성화 후 즉시 컨트롤
+  self.clients.claim(); 
 });
